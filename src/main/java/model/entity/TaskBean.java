05adbf462538d94e2ae8f8086155d6b230d3041d
emtech-koushi <emtech-koushi@PC-name>
2025-03-24 15:07:19 +0900

    TaskBeanにコンストラクタとgetter、setterを追加

--- src/main/java/model/entity/TaskBean.java
+++ src/main/java/model/entity/TaskBean.java
@@ -10,4 +10,95 @@ public class TaskBean implements Serializable {
 	private String userId;
 	private String statusCode;
 	private String memo;
+	
+	// コンストラクタ
+	public TaskBean() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * @return taskName
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+
+	/**
+	 * @param taskName セットする taskName
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	/**
+	 * @return categoryId
+	 */
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	/**
+	 * @param categoryId セットする categoryId
+	 */
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	/**
+	 * @return limitDate
+	 */
+	public Date getLimitDate() {
+		return limitDate;
+	}
+
+	/**
+	 * @param limitDate セットする limitDate
+	 */
+	public void setLimitDate(Date limitDate) {
+		this.limitDate = limitDate;
+	}
+
+	/**
+	 * @return userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return statusCode
+	 */
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	/**
+	 * @param statusCode セットする statusCode
+	 */
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	/**
+	 * @return memo
+	 */
+	public String getMemo() {
+		return memo;
+	}
+
+	/**
+	 * @param memo セットする memo
+	 */
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
+	
 }
