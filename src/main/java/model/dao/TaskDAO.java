33ae0bfb01fa797784b5a6b8acedd512e5ae6979
emtech-koushi <emtech-koushi@PC-name>
2025-03-24 16:11:45 +0900

    タスク一覧の取得を追加

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.entity.CategoryBean;
+import model.entity.TaskBean;
 
 public class TaskDAO {
 	/**
@@ -31,5 +32,39 @@ public class TaskDAO {
 			}
 		}
 		return categoryList;
+	}
+	
+	/**
+	 * 
+	 * @return タスク一覧の取得
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public List<TaskBean> getTaskList() throws ClassNotFoundException, SQLException {
+		List<TaskBean> taskList = new ArrayList<TaskBean>();
+		String sql = "SELECT t1.task_id, t1.task_name, t1.category_id, t2.category_name, t1.limit_date, t1.user_id, t3.user_name, t1.status_code, t4.status_name, t1.memo FROM t_task t1 INNER JOIN m_category t2 ON t1.category_id = t2.category_id INNER JOIN m_user t3 ON t1.user_id = t3.user_id INNER JOIN m_status t4 ON t1.status_code = t4.status_code ORDER BY task_id asc";
+		
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+			ResultSet res = pstmt.executeQuery();
+			
+			while(res.next()) {
+				TaskBean task = new TaskBean();
+				task.setTaskId(res.getInt("t1.task_id"));
+				task.setTaskName(res.getString("t1.task_name"));
+				task.setCategoryId(res.getInt("t1.category_id"));
+				task.setCategoryName(res.getString("t2.category_name"));
+				task.setLimitDate(res.getDate("t1.limit_date"));
+				task.setUserId(res.getString("t1.user_id"));
+				task.setUserName(res.getString("t3.user_name"));
+				task.setStatusCode(res.getString("t1.status_code"));
+				task.setStatusName(res.getString("t4.status_name"));
+				task.setMemo(res.getString("t1.memo"));
+				
+				taskList.add(task);
+			}
+		}
+		
+		return taskList;
 	}
 }
